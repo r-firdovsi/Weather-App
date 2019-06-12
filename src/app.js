5a838c56289975b1dbeb39e608d761e113fd7dd3
r-firdovsi <r-firdovsi@hotmail.com>
2019-06-12 15:02:45 +0400

    Remove unnecessary console.log call

--- src/app.js
+++ src/app.js
@@ -68,22 +68,6 @@ app.get('/weather', (req, res) => {
     })
 })
 
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        return res.send({
-            error: 'You must provided a search term'
-        })
-    } else {
-
-    }
-
-
-    console.log(req.query.search)
-    res.send({
-        products: []
-    })
-})
-
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
